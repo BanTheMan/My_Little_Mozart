7355ce49fc89ae98f4f11edaec8339ea27f8b070
ngsk6y <ngsk6y@umsystem.edu>
2025-04-02 17:39:12 -0500

    Pitch being weird
    updating pitch because its being weird

--- My_Little_Mozart/src/symphony/strategy/LowerPitchStrategy.java
+++ My_Little_Mozart/src/symphony/strategy/LowerPitchStrategy.java
@@ -3,7 +3,8 @@ package symphony.strategy;
 public class LowerPitchStrategy implements PitchStrategy {
 
 	@Override
-	public int modifyPitch(int note ) {
+	public int modifyPitch(int note) {
 		return note - 2;
 	}
+	
 }
