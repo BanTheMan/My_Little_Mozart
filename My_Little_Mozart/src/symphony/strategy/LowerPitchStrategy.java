3fbb2420782ef41533e57d4c2c37d433b27a99a1
ngsk6y <ngsk6y@umsystem.edu>
2025-04-02 17:31:56 -0500

    Pitching Strategies
    Added pitch strategies for lower and higher

--- My_Little_Mozart/src/symphony/strategy/LowerPitchStrategy.java
+++ My_Little_Mozart/src/symphony/strategy/LowerPitchStrategy.java
@@ -1,5 +1,9 @@
 package symphony.strategy;
 
-public class LowerPitchStrategy {
+public class LowerPitchStrategy implements PitchStrategy {
 
+	@Override
+	public int modifyPitch(int note ) {
+		return note - 2;
+	}
 }
