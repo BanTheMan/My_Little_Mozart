d461c360b9154da1d6e93f1a49f6f3650edb7eeb
BanTheMan <brandonmgomes06@gmail.com>
2025-04-03 17:11:32 -0500

    Implemented Factories and their Abstract Counterparts

--- My_Little_Mozart/src/symphony/factory/LegatoMidiEventFactory.java
+++ My_Little_Mozart/src/symphony/factory/LegatoMidiEventFactory.java
@@ -1,5 +1,21 @@
 package symphony.factory;
 
-public class LegatoMidiEventFactory {
+import javax.sound.midi.*;
+
+public class LegatoMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		return new MidiEvent(message, tick+80);
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+		return new MidiEvent(message, tick+80);
+	}
 
 }
--- My_Little_Mozart/src/symphony/factory/StandardMidiEventFactory.java
+++ My_Little_Mozart/src/symphony/factory/StandardMidiEventFactory.java
@@ -1,5 +1,21 @@
 package symphony.factory;
 
-public class StandardMidiEventFactory {
+import javax.sound.midi.*;
+
+public class StandardMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		return new MidiEvent(message, tick);
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_ON, channel, note, 0);
+		return new MidiEvent(message, tick);
+	}
 
 }
