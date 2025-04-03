f233673c6872669650023c4ab63d61b1e811b5ac
BanTheMan <brandonmgomes06@gmail.com>
2025-04-03 16:25:14 -0500

    Visual tweek

--- My_Little_Mozart/src/symphony/Main.java
+++ My_Little_Mozart/src/symphony/Main.java
@@ -17,40 +17,40 @@ public class Main {
 			MidiEventFactoryAbstract factoryAbstract = new StandardMidiEventFactoryAbstract();
 			// MidiEventFactoryAbstract factoryAbstract = new LegatoMidiEventFactoryAbstract();
 			// MidiEventFactoryAbstract factoryAbstract = new StaccatoMidiEventFactoryAbstract();
-			MidiEventFactory factory = factoryAbstract.createFactory ();
+			MidiEventFactory factory = factoryAbstract.createFactory();
 			// Choose an instrument strategy (e.g. , Trumpet, Bass Guitar , Piano)
 			InstrumentStrategy instrumentStrategy = new ElectricBassGuitarStrategy();
 			instrumentStrategy.applyInstrument(track, 0);
 			instrumentStrategy = new TrumpetStrategy();
 			instrumentStrategy.applyInstrument(track, 1);
 			// Choose a pitch strategy (e.g. , HigherPitch , LowerPitch)
-			PitchStrategy pitchStrategy = new HigherPitchStrategy ();
+			PitchStrategy pitchStrategy = new HigherPitchStrategy();
 			for (MidiEventData event : midiEvents) {
-				int modifiedNote = pitchStrategy .modifyPitch(event . getNote ());
+				int modifiedNote = pitchStrategy.modifyPitch(event.getNote ());
 				// call this as much as you want if you want to get a higher pitch
-				modifiedNote = pitchStrategy . modifyPitch(modifiedNote );
-				if ( event.getNoteOnOff() == ShortMessage.NOTE_ON) {
-					track.add( factory . createNoteOn(event . getStartEndTick () ,
-							modifiedNote ,
-							event.getVelocity () ,
-							event.getChannel ()));
+				modifiedNote = pitchStrategy.modifyPitch(modifiedNote);
+				if (event.getNoteOnOff() == ShortMessage.NOTE_ON) {
+					track.add( factory.createNoteOn(event.getStartEndTick(),
+							modifiedNote,
+							event.getVelocity(),
+							event.getChannel()));
 				}
 				else {
-					track .add( factory . createNoteOff(event . getStartEndTick () , modifiedNote , event . getChannel ()));
+					track.add(factory.createNoteOff(event.getStartEndTick(), modifiedNote, event.getChannel()));
 				}
 			}
 			// Playing the sequence
-			Sequencer sequencer = MidiSystem. getSequencer ();
-			sequencer .open ();
-			sequencer . setSequence(sequence );
-			sequencer . start ();
-			while (sequencer . isRunning() | sequencer .isOpen()) {
-				Thread. sleep (100);
+			Sequencer sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			sequencer.setSequence(sequence);
+			sequencer.start();
+			while (sequencer.isRunning() | sequencer.isOpen()) {
+				Thread.sleep (100);
 			}
-			Thread. sleep (500);
-			sequencer . close ();
+			Thread.sleep (500);
+			sequencer.close();
 		} catch (Exception e) {
-			e . printStackTrace ();
+			e.printStackTrace();
 		}
 	}
 }
